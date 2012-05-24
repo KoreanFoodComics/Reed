7fa29f4e567537fa4f9839c68f221e238c0c3d90
The Babblery <the@babblery.com>
2012-05-23 19:53:31 -0700

    actually did what I said in the last commit
    Sorry, just getting the hang of this "git" thing.

--- src/com/babblery/reed/scene/Component.groovy
+++ src/com/babblery/reed/scene/Component.groovy
@@ -3,6 +3,44 @@ package com.babblery.reed.scene
 import org.newdawn.slick.GameContainer
 import org.newdawn.slick.Graphics
 
-interface Component {
-    void render(GameContainer container, Graphics g)
+abstract class Component {
+    /**
+     * The parent of this component.
+     */
+    Scene parentScene
+
+    /**
+     * Create a new component with the specified parent scene.
+     *
+     * @param parentScene The parent of this component.
+     */
+    Component(Scene parentScene) {
+        this.parentScene = parentScene
+    }
+
+    /**
+     * Get the parent scene for this component.
+     *
+     * @return The parent scene for this component.
+     */
+    Scene getParentScene() {
+        return parentScene
+    }
+
+    /**
+     * Set the parent scene for this component.
+     *
+     * @param parentScene The new parent scene.
+     */
+    void setParentScene(Scene parentScene) {
+        this.parentScene = parentScene
+    }
+
+    /**
+     * Called every frame this component is visible.
+     *
+     * @param container The Slick GameContainer for this game.
+     * @param g The Slick Graphics object (nearly identical to {@link java.awt.Graphics2D}).
+     */
+    abstract void render(GameContainer container, Graphics g)
 }
